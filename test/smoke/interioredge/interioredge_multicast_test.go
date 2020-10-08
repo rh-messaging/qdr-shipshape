eff56459687afa6421b5bf304f53225e06f0fd44
Renato Granzoto <rgranzot@redhat.com>
2020-10-08 09:17:32 -0300

    Remove unnecessary code
    Signed-off-by: Renato Granzoto <rgranzot@redhat.com>

--- test/smoke/interioredge/interioredge_multicast_test.go
+++ test/smoke/interioredge/interioredge_multicast_test.go
@@ -22,8 +22,6 @@ var _ = Describe("Exchange MultiCast messages across all nodes", func() {
 
 	if os.Getenv("IMAGE_QDROUTERD_INTEROP") != "" {
 		testSufix = " - Using Interoperability mode"
-	} else {
-		testSufix = ""
 	}
 
 	It(fmt.Sprintf("exchanges %d small messages with 1kb using senders and receivers across all router nodes%s", totalSmall, testSufix), func() {
